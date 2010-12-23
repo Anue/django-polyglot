0e4dcef872da4b6c5f828c816e47dc18d431d818
Marcos Moyano <marcos@anue.biz>
2010-12-23 12:36:24 -0300

    Create translation files for es lang

--- example/settings.py
+++ example/settings.py
@@ -33,6 +33,8 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
+LOCALE_PATHS = ('locale/',) 
+
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
